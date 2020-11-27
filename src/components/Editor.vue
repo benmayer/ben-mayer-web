0034692fbd59027974a2e48fab0e41b1e79782d4
Ben Mayer <benmayer@bens-mbp.speedport_w_724v_09011603_06_007>
2020-11-28 00:24:06 +0100

    Working /edit page with html markup

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="editor">
-    <!-- <editor-menu-bar v-slot="{ commands, isActive, getMarkAttrs }" :editor="editor"> -->
-    <editor-menu-bar v-slot="{ }" :editor="editor">
-      <!-- <div class="menubar flex flex-wrap items-center mb-2">
+    <editor-menu-bar v-slot="{commands, isActive, getMarkAttrs }" :editor="editor">
+      <div class="menubar flex flex-wrap items-center mb-2">
         <div class="menubar__group">
           <button
             :class="{ 'is-active': isActive.bold() }"
             class="menubar__button"
             @click="commands.bold"
           >
-            <icon-format-bold class="menubar__icon" />
+            <b>B</b>
           </button>
           <button
             :class="{ 'is-active': isActive.italic() }"
             class="menubar__button"
             @click="commands.italic"
           >
-            <icon-format-italic class="menubar__icon" />
+            <i>I</i>
           </button>
           <button
             :class="{ 'is-active': isActive.strike() }"
             class="menubar__button"
             @click="commands.strike"
           >
-            <icon-format-strikethrough class="menubar__icon" />
+            <s>S</s>
           </button>
           <button
             :class="{ 'is-active': isActive.underline() }"
             class="menubar__button"
             @click="commands.underline"
           >
-            <icon-format-underline class="menubar__icon" />
+          <u>U</u>
           </button>
           <button
             :class="{ 'is-active': isActive.code() }"
             class="menubar__button"
             @click="commands.code"
           >
-            <icon-format-code class="menubar__icon" />
+           <span>< ></span>
           </button>
         </div>
         <div class="menubar__group">
+            <button
+            :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+            class="menubar__button"
+            @click="commands.heading({ level: 1 })"
+          >
+            H1
+          </button>
           <button
             :class="{ 'is-active': isActive.heading({ level: 2 }) }"
             class="menubar__button"
@@ -55,13 +61,7 @@
           >
             H3
           </button>
-          <button
-            :class="{ 'is-active': isActive.heading({ level: 4 }) }"
-            class="menubar__button"
-            @click="commands.heading({ level: 4 })"
-          >
-            H4
-          </button>
+
         </div>
         <div class="menubar__group">
           <button
@@ -69,14 +69,14 @@
             class="menubar__button"
             @click="commands.bullet_list"
           >
-            <icon-format-list-bulleted class="menubar__icon" />
+            <span>ul</span>
           </button>
           <button
             :class="{ 'is-active': isActive.ordered_list() }"
             class="menubar__button"
             @click="commands.ordered_list"
           >
-            <icon-format-list-numbered class="menubar__icon" />
+            <span>ol</span>
           </button>
         </div>
         <div class="menubar__group">
@@ -85,7 +85,7 @@
             class="menubar__button"
             @click="showLinkPrompt(commands.link, getMarkAttrs('link'))"
           >
-            <icon-link class="menubar__icon" />
+            🔗
           </button>
           <button
             :disabled="!isActive.link()"
@@ -93,33 +93,33 @@
             class="menubar__button"
             @click="removeLink(commands.link)"
           >
-            <icon-link-off class="menubar__icon" />
+            <!-- <icon-link-off class="menubar__icon" /> -->
           </button>
           <button
             class="menubar__button"
             @click="showImagePrompt(commands.image)"
           >
-            <icon-add-photo class="menubar__icon" />
+            □
           </button>
           <button
             :class="{ 'is-active': isActive.blockquote() }"
             class="menubar__button"
             @click="commands.blockquote"
           >
-            <icon-format-quote class="menubar__icon" />
+            <span>❝❞</span>
           </button>
           <button
             :class="{ 'is-active': isActive.code_block() }"
             class="menubar__button"
             @click="commands.code_block"
           >
-            <icon-format-code class="menubar__icon" />
+            <span><></span>
           </button>
           <button
             class="menubar__button"
             @click="commands.horizontal_rule"
           >
-            <icon-remove class="menubar__icon" />
+            <span>—</span>
           </button>
         </div>
         <div class="menubar__group">
@@ -127,85 +127,47 @@
             class="menubar__button"
             @click="commands.undo"
           >
-            <icon-undo class="menubar__icon" />
+            <span>⟲</span>
           </button>
           <button
             class="menubar__button"
             @click="commands.redo"
           >
-            <icon-redo class="menubar__icon" />
+          <span>⟳</span>
+            <!-- <icon-redo class="menubar__icon" /> -->
           </button>
         </div>
-      </div> -->
+      </div>
     </editor-menu-bar>
     <editor-content :editor="editor" class="editor__content content" />
   </div>
 </template>
 
 <script>
-// import bash from 'highlight.js/lib/languages/bash'
-// import css from 'highlight.js/lib/languages/css'
-// import javascript from 'highlight.js/lib/languages/javascript'
-// import php from 'highlight.js/lib/languages/php'
-// import shell from 'highlight.js/lib/languages/shell'
-// import sql from 'highlight.js/lib/languages/sql'
-// import xml from 'highlight.js/lib/languages/xml'
-// import yaml from 'highlight.js/lib/languages/yaml'
 import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
-// import {
-//   Blockquote,
-//   CodeBlock,
-//   CodeBlockHighlight,
-//   HardBreak,
-//   Heading,
-//   HorizontalRule,
-//   OrderedList,
-//   BulletList,
-//   ListItem,
-//   TodoItem,
-//   TodoList,
-//   Bold,
-//   Code,
-//   Italic,
-//   Link,
-//   Underline,
-//   Strike,
-//   History,
-//   Image,
-//   Placeholder
-// } from 'tiptap-extensions'
-// import IconFormatBold from '~/components/icons/IconFormatBold'
-// import IconFormatItalic from '~/components/icons/IconFormatItalic'
-// import IconFormatUnderline from '~/components/icons/IconFormatUnderline'
-// import IconFormatStrikethrough from '~/components/icons/IconFormatStrikethrough'
-// import IconFormatListBulleted from '~/components/icons/IconFormatListBulleted'
-// import IconFormatListNumbered from '~/components/icons/IconFormatListNumbered'
-// import IconFormatCode from '~/components/icons/IconFormatCode'
-// import IconFormatQuote from '~/components/icons/IconFormatQuote'
-// import IconRemove from '~/components/icons/IconRemove'
-// import IconUndo from '~/components/icons/IconUndo'
-// import IconRedo from '~/components/icons/IconRedo'
-// import IconAddPhoto from '~/components/icons/IconAddPhoto'
-// import IconLink from '~/components/icons/IconLink'
-// import IconLinkOff from '~/components/icons/IconLinkOff'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
 
 export default {
   name: 'Editor',
   components: {
-    // IconLinkOff,
-    // IconLink,
-    // IconAddPhoto,
-    // IconRedo,
-    // IconUndo,
-    // IconRemove,
-    // IconFormatQuote,
-    // IconFormatCode,
-    // IconFormatListNumbered,
-    // IconFormatListBulleted,
-    // IconFormatStrikethrough,
-    // IconFormatUnderline,
-    // IconFormatItalic,
-    // IconFormatBold,
     EditorContent,
     EditorMenuBar
   },
@@ -223,55 +185,37 @@ export default {
       html: ''
     }
   },
-  mounted () {
-    this.html = this.value
+    mounted () {
+      this.html = this.value
+      this.editor = new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: this.html,
+        onUpdate: ({ getHTML }) => {
+            this.html = getHTML()
+            this.$emit('input', this.html)
 
-    this.editor = new Editor({
-    // //   extensions: [
-    // //     new Blockquote(),
-    // //     new BulletList(),
-    // //     new CodeBlock(),
-    // //     new CodeBlockHighlight({
-    // //       languages: {
-    // //         javascript,
-    // //         css,
-    // //         xml,
-    // //         php,
-    // //         yaml,
-    // //         sql,
-    // //         bash,
-    // //         shell
-    // //       }
-    // //     }),
-    // //     new HardBreak(),
-    // //     new Heading({ levels: [2, 3, 4] }),
-    // //     new HorizontalRule(),
-    // //     new ListItem(),
-    // //     new OrderedList(),
-    // //     new TodoItem(),
-    // //     new TodoList(),
-    // //     new Link(),
-    // //     new Bold(),
-    // //     new Code(),
-    // //     new Italic(),
-    // //     new Underline(),
-    // //     new Strike(),
-    // //     new History(),
-    // //     new Image(),
-    // //     new Placeholder({
-    // //       emptyEditorClass: 'is-editor-empty',
-    // //       emptyNodeClass: 'is-empty',
-    // //       emptyNodeText: 'Write...',
-    // //       showOnlyWhenEditable: true,
-    // //       showOnlyCurrent: true
-    // //     })
-    //   ],
-      content: this.html,
-      onUpdate: ({ getHTML }) => {
-        this.html = getHTML()
-        this.$emit('input', this.html)
-      }
-    })
+            console.log(this.value)
+        },
+      }),
+    console.log(this.value)
   },
   beforeDestroy () {
     this.editor.destroy()
@@ -298,7 +242,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .editor {
   display: flex;
   flex-direction: column;
@@ -309,13 +253,19 @@ export default {
   flex: 0 1 auto;
 }
 
-.editor__content {
+.edtor__content {
   flex: 1 1 auto;
   overflow: auto;
+  cursor: text;
+}
+.editor__content:focus, 
+.editor__content *:focus {
+    border: none;
+    outline: none;
 }
 
-/* .menubar__button {
-  @apply p-2 mb-2 bg-gray-300 font-bold rounded;
+.menubar__button {
+  @apply p-2 mb-2 bg-gray-400 text-black font-bold rounded;
   min-width: 40px;
 }
 
@@ -356,5 +306,21 @@ export default {
 
 .menubar__group .menubar__button:last-child {
   @apply rounded-r mr-2;
-} */
+}
+
+
+.editor__content * {
+    caret-color: currentColor;
+}
+
+.editor__content blockquote {
+    border-left: 3px solid rgb(171 171 171 / 28%);;
+    padding-left: .8rem;
+    font-style: italic;
+}
+
+.editor__content ol, 
+.editor__content ul {
+    padding-left: 1rem;
+}
 </style>
